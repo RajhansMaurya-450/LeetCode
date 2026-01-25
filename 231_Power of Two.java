ea39db1f2fd725afd54186d5d3bfa60aa51ecaa7
Raj <rajmaurya3091@gmail.com>
2026-01-25 23:25:20 +0530

    Que231_power_of_two

---
+++ 231_Power of Two.java
@@ -0,0 +1,21 @@
+public class 231_Power of Two {
+    class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n==1) {
+            return true;
+        }
+        if(n==0){
+            return false;
+        }
+        while(n % 2 == 0){
+            n = n / 2;
+        }
+        if(n==1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
+}
