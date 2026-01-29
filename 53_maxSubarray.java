d3a1c4be166cb5f458fc1984604ea2fea936f33f
Raj <rajmaurya3091@gmail.com>
2026-01-29 23:59:00 +0530

    53_leetcode_maxSub_array(kadane's algorithm)

---
+++ 53_maxSubarray.java
@@ -0,0 +1,20 @@
+public class 53_maxSubarray {
+    class Solution {
+    public int maxSubArray(int[] nums) {
+        int currentSum = 0;
+        int maxSum =Integer.MIN_VALUE;
+        
+        for(int i = 0; i  < nums.length; i++){
+           
+            currentSum += nums[i];
+             maxSum = Math.max(currentSum,maxSum);
+            if(currentSum < 0) {
+                currentSum = 0;
+            }
+        }
+        return maxSum;
+    }
+}
+}
+// Que53: leetcode maxSubArray using kadane's algorithm
+// if current sum < -ve then cs = 0;
