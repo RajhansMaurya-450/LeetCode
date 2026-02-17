475acca952c4e651e1f8394a698022ea6929345c
Raj <rajmaurya3091@gmail.com>
2026-02-17 23:58:10 +0530

    405_max_consecutive_ones

---
+++ 405_max_consecutive_ones.java
@@ -0,0 +1,17 @@
+public class 405_max_consecutive_ones {
+    class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+       int count = 0;
+        int maxCount = 0;
+        for(int num:nums){
+            if(num == 1){
+                count++ ;
+                maxCount = Math.max(maxCount,count);
+            } else {
+                count = 0;
+            }
+        }
+        return maxCount;
+    }
+}
+}
