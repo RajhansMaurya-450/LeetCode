9e943a64b2f103b56003ff8697fa2ec324dc7f6a
Raj <rajmaurya3091@gmail.com>
2026-03-13 23:59:41 +0530

    2149_leetcode_reaarange_element_by_sign

---
+++ 2149_rearrange_element_by_sign.java
@@ -0,0 +1,23 @@
+public class 2149_rearrange_element_by_sign {
+    class Solution {
+    public int[] rearrangeArray(int[] nums) {
+         int[] result = new int[nums.length];
+
+        int posIndex = 0;
+        int negIndex = 1;
+
+        for (int num : nums) {
+
+            if (num > 0) {
+                result[posIndex] = num;
+                posIndex += 2;
+            } else {
+                result[negIndex] = num;
+                negIndex += 2;
+            }
+        }
+
+        return result;
+    }
+}
+}
