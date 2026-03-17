6b8bbdb8fa6524bcf12a106644bb135fd35248a2
Raj <rajmaurya3091@gmail.com>
2026-03-17 23:50:40 +0530

    1052_leetcode_grumpy_bookstore_owner

---
+++ 1052_grumpy_bookStore.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
+         int n = customers.length;
+
+        // Step 1: already satisfied
+        int base = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (grumpy[i] == 0) {
+                base += customers[i];
+            }
+        }
+
+        // Step 2: sliding window for extra gain
+        int extra = 0;
+
+        // first window
+        for (int i = 0; i < minutes; i++) {
+            if (grumpy[i] == 1) {
+                extra += customers[i];
+            }
+        }
+
+        int maxExtra = extra;
+
+        // slide window
+        for (int i = minutes; i < n; i++) {
+
+            if (grumpy[i] == 1) {
+                extra += customers[i];
+            }
+
+            if (grumpy[i - minutes] == 1) {
+                extra -= customers[i - minutes];
+            }
+
+            maxExtra = Math.max(maxExtra, extra);
+        }
+
+        return base + maxExtra;
+    }
+}
