22e3af57c13488b368be7da6012e477080a8dca0
Raj <rajmaurya3091@gmail.com>
2026-03-23 23:56:03 +0530

    719_leetcode_subarr_product_less_than_K

---
+++ 713_subarr_prod_less_than_k.java
@@ -0,0 +1,25 @@
+public class 713_subarr_prod_less_than_k {
+    class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+         if (k <= 1) return 0;
+
+        int left = 0;
+        int product = 1;
+        int count = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+            product *= nums[right];
+
+            while (product >= k) {
+                product /= nums[left];
+                left++;
+            }
+
+            count += (right - left + 1);
+        }
+
+        return count;
+    }
+}
+}
+//sliding windows problem
