a88712d2326e374d9c960d7ee9679abcc18f2725
Raj <rajmaurya3091@gmail.com>
2026-04-25 23:44:43 +0530

    2053_kth_distinct_string_in_an_arr

---
+++ 2053_ Kth Distinct String in an Array.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String kthDistinct(String[] arr, int k) {
+          Map<String, Integer> freq = new HashMap<>();
+
+        // Count frequency
+        for (String s : arr) {
+            freq.put(s, freq.getOrDefault(s, 0) + 1);
+        }
+
+        // Find k-th distinct
+        int count = 0;
+        for (String s : arr) {
+            if (freq.get(s) == 1) {
+                count++;
+                if (count == k) {
+                    return s;
+                }
+            }
+        }
+
+        return "";
+    }
+}
