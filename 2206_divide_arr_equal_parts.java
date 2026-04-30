c1534db02eb6a54b05f00200882722cfa0246a33
Raj <rajmaurya3091@gmail.com>
2026-05-01 01:05:56 +0530

    2206_leetcode Divide Array Into Equal Pairs

---
+++ 2206_divide_arr_equal_parts.java
@@ -0,0 +1,19 @@
+public class 2206_divide_arr_equal_parts {
+    class Solution {
+    public boolean divideArray(int[] nums) {
+        int[] freq = new int[501]; // constraints: nums[i] <= 500
+
+        for (int num : nums) {
+            freq[num]++;
+        }
+
+        for (int count : freq) {
+            if (count % 2 != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+}
