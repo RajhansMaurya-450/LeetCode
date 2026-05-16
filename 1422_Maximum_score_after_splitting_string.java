e94d56d9c9a93d61177fcc103029dabcb7d298eb
Raj <rajmaurya3091@gmail.com>
2026-05-16 23:55:12 +0530

    1422_max_score_after_splitting_string

---
+++ 1422_Maximum_score_after_splitting_string.java
@@ -0,0 +1,31 @@
+public class 1422_Maximum_score_after_splitting_string {
+    class Solution {
+    public int maxScore(String s) {
+        int ones = 0;
+
+        // Count total ones
+        for (char c : s.toCharArray()) {
+            if (c == '1') {
+                ones++;
+            }
+        }
+
+        int zeros = 0;
+        int maxScore = 0;
+
+        // Split before last character
+        for (int i = 0; i < s.length() - 1; i++) {
+
+            if (s.charAt(i) == '0') {
+                zeros++;
+            } else {
+                ones--;
+            }
+
+            maxScore = Math.max(maxScore, zeros + ones);
+        }
+
+        return maxScore;  
+    }
+}
+}
